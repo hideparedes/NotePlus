29ddbd9748acbeb6f9df4ba8f06cb56503bb9ca9
Hidenori Kawano <hidenori.kawano@gmail.com>
2020-01-25 18:32:36 +0900

    Prepare for deploy

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 module.exports = async () => {
   try {
-    await mongoose.connect("mongodb://localhost:27017/keeperDB", {
+    await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
       useFindAndModify: false
--- server.js
+++ server.js
@@ -3,6 +3,7 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const connectDB = require("./config/db");
+const path = require("path")
 
 connectDB();
 
@@ -12,6 +13,14 @@ app.use(express.json({
 
 app.use("/api/users", require("./routes/users"));
 app.use("/api/notes", require("./routes/notes"));
+
+// Serve static assets in production
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => res.sendFile(path.resolve(__dirname, "client", "build", "index.html")));
+}
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
